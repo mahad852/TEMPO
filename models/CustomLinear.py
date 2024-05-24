cd8c8382706dfc0591f0520922710c4db75e4aa9
Mahad <mahad852@gmail.com>
2024-05-25 02:31:09 +0500

    removed debug logs

--- models/CustomLinear.py
+++ models/CustomLinear.py
@@ -14,6 +14,5 @@ class CustomLinear(nn.Module):
         self.output_layer = nn.Linear(self.embed_dim, self.pred_len)
 
     def forward(self, batch_x):
-        print(batch_x.shape)
         x = self.relu1(self.input_layer(batch_x.squeeze(-1)))
         return self.output_layer(x).unsqueeze(-1)
